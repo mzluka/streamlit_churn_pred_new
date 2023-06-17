1b0dad07ac53920fa9d6d3008c067ba93dbce1e5
Marina <134020308+mzluka@users.noreply.github.com>
2023-06-17 11:50:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def set_png_as_page_bg(png_file):
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-set_png_as_page_bg('7.jpg')
+set_png_as_page_bg('6.jpg')
 
 
 classifier_name=['LightGMB', 'RandomForest', 'LogisticRegression']
